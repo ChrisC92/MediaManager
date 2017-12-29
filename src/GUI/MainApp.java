2380386a5f90456e9d8a8c36d28a531929d2fd0a
Christopher Corner <c.corner@live.co.uk>
2017-12-29 23:29:48 +0000

    Initial layout of the GUI created in the GUI class
    Following MVC principles to bring the app together

---
+++ src/GUI/MainApp.java
@@ -0,0 +1,68 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("SeriesTracker");
+
+//        setUserAgentStylesheet(STYLESHEET_MODENA);
+
+        initRootLayout();
+        showPersonOverView();
+    }
+
+    /**
+     *  RootLayout contains the menu bar UI
+     */
+    private void initRootLayout() {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *  Shows the main layout inside of the root layout
+     */
+    private void showPersonOverView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/MainLayout.fxml"));
+            AnchorPane mainLayout = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(mainLayout);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
